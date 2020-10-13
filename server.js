b781d1a3d6f631e1e9c68b39862a155df981e6a7
MarceHM <marcehormazamiranda@gmail.com>
2020-10-13 13:59:58 -0500

    Web server without DB connection

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 5000;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname + '/webserver/')));
 
 //Routes
 app.get('/', (req, res) => {
-        res.sendFile('../index.html');
+        res.sendFile(path.join(__dirname + '/webserver/index.html'));
     });
 
 app.post('/submit-data', (req, res) => {
