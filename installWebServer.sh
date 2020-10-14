98144518d7c961db64f02696e398368d433ae0ad
Marcela Hormaza <47231554+MarceHM@users.noreply.github.com>
2020-10-14 15:31:29 -0500

    Update installWebServer.sh

--- installWebServer.sh
+++ installWebServer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo yum update
+sudo apt-get update
 
 #Installing Node.js
 #Source: https://github.com/nodesource/distributions
-curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-sudo yum install –y nodejs
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 
 #Installing node modules
 npm i --save express body-parser path mongoose
