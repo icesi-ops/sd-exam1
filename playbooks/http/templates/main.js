23601bcedc575a8af608593bd78564d9e328c428
paola <paola.a.veloza@gmail.com>
2021-03-16 19:12:50 -0500

    Create a js file to consume backend

---
+++ playbooks/http/templates/main.js
@@ -0,0 +1,18 @@
+
+
+/*
+fetch(url)
+.then(response=> response.json())
+.then(data => {
+    let element = document.getElementById('element')
+    element.innerHTML= '/index.j2'
+    console.log(data)
+})
+.catch(err=>console)
+*/
+let url = "http//:192.168.33.200"
+$.get(url, function(answer){
+    //let list = answer
+
+    console.
+},"json")
