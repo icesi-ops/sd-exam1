59e33b9a5b1ad70b4c01b8562b41f1e2f19dd256
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2021-03-17 19:29:17 -0500

    docker fixed

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import {Request, Response} from 'express'
 
-import * as Busboy from "busboy";
+import Busboy from "busboy";
 import * as path from "path";
 import * as fs from "fs";
+import * as env from 'env-var';
 
 const df = require('node-df');
 const util = require('util'); 
 const real_df = util.promisify(df);
+
+
+const storage: string = env.get('STORAGE').required().asString()
 
 const router = express.Router()
 
@@ -16,7 +20,20 @@ router.get('/api/files', (req: Request, res: Response) => {
 })
 
 router.post('/api/upload', (req: Request, res: Response) => {
-    return res.sendStatus(200)
+    const busboy = new Busboy({ headers: req.headers });
+
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+
+        // path to file upload
+        const saveTo = path.join(storage, filename);
+
+        file.pipe(fs.createWriteStream(saveTo));
+    });
+
+    busboy.on("finish", function () {
+        res.status(200).json({ "message": "File uploaded successfully." });
+    });
+    req.pipe(busboy);
 })
 
 router.get('/api/availableStorage', async (req: Request, res: Response) => {
