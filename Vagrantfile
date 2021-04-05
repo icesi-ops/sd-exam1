91ee8b44c506ea367a51594f682d73b1189bc118
DanielCerquera13 <danielalejandrocerquera@gmail.com>
2021-04-05 01:58:56 -0500

    Configure Web Server and DataBase for Upload Files

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,6 @@
 
 Vagrant.configure("2") do |config|
 
-	config.ssh.insert_key = false
-
 	config.vm.define "loadbalancer" do |loadbalancer|
            loadbalancer.vm.box = "centos/7"
 	   loadbalancer.vm.hostname = "loadbalancer"
@@ -15,18 +13,19 @@ Vagrant.configure("2") do |config|
 	      ansible.extra_vars = {
 	         "web_servers" => [
                    {"name": "webserver-1", "ip":"192.168.33.11"},
-	               {"name": "webserver-2", "ip":"192.168.33.12"}
+	            {"name": "webserver-2", "ip":"192.168.33.12"}
                   ]
                  }
             end
            end
+           config.ssh.insert_key = false
            (1..2).each do |i|
             config.vm.define "webserver-#{i}" do |webserver|
              webserver.vm.box = "centos/7"
 	     webserver.vm.hostname = "webserver-#{i}"
 	     webserver.vm.network "private_network", ip: "192.168.33.1#{i}"
 	     webserver.vm.provider "virtualbox" do |vb|
-	      vb.customize  ["modifyvm", :id, "--memory", "512", "--cpus", "1", "--name", "webserver-#{i}" ]
+	      vb.customize  ["modifyvm", :id, "--memory", "512", "--cpus", "1", "--name", "webserver-#{i}"]
 	      unless File.exist?("./Disco#{i}.vdi")
           vb.customize ['createhd', '--filename', "./Disco#{i}.vdi", '--variant', 'Fixed', '--size', 2 * 1024]
 	      end
--- playbooks/httpd/templates/webserver.py
+++ playbooks/httpd/templates/webserver.py
@@ -1,39 +1,56 @@
 # Imports
 import os
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify, Response, flash, redirect
 from flask_pymongo import PyMongo
 from bson import json_util
 from bson.objectid import ObjectId
+from flask_mongoengine import MongoEngine
 import json
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from waitress import serve
+import urllib.request
 
 # instancia del objeto Flask
 app = Flask(__name__)
 app.secret_key = 'myawesomesecretkey'
-app.config['MONGO_URI'] = 'mongodb://192.168.33.100:27017/pythonmongodb'
-mongo = PyMongo(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'local',
+    'host': '192.168.33.100',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)  
 
-# Carpeta de subida
-#app.config['UPLOAD_FOLDER'] = './subidos'
+
+#UPLOAD FOLDER
+UPLOAD_FOLDER = 'static/img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+ return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
 def upload_file():
- # renderiamos la plantilla "formulario.html"
- return render_template('index.j2')
+  return render_template('index.j2')
 
 @app.route("/upload", methods=['POST'])
 def uploader():
- if request.method == 'POST':
-  # obtenemos el archivo del input "archivo"
-  f = request.files['archivo']
-  filename = secure_filename(f.filename)
-  # Guardamos el archivo en el directorio "Archivos PDF"
-  f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  # Retornamos una respuesta satisfactoria
-  return "<h1>Archivo subido exitosamente</h1>"
+    file = request.files['inputFile']
+    filename = file.filename
+
+    if file and allowed_file(file.filename):
+       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+       flash('File successfully uploaded ' + file.filename + ' to the database!')
+       return redirect('/')
+    else:
+       flash('Invalid Uplaod only txt, pdf, png, jpg, jpeg, gif')
+    return redirect('/')
 
 if __name__ == '__main__':
  # Iniciamos la aplicación
-    serve(app, host="0.0.0.0", port="80")
+    app.run(host="0.0.0.0", port="80")
