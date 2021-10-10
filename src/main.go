357979fc777d2a9c147064431aa076ab3fb5aa77
symghoul <1192914079@u.icesi.edu.co>
2021-10-09 21:24:04 -0500

    Firsts Go configurations

--- src/main.go
+++ src/main.go
@@ -9,9 +9,22 @@ import (
 
 func main() {
   handler := handlers.NewHandler()
+  
+  http.HandleFunc("/", home)
 
 	http.HandleFunc("/upload", handler.UploadFilesHandler)
   http.HandleFunc("/files", handler.FetchFilesHandler)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func home(w http.ResponseWriter, r *http.Request){
+
+	html := "<html>";
+	html += "<body>";
+	html += "<h1>Hola Mundo </h1>";
+	html += "</body>";
+	html += "</html>";
+	
+	w.Write([]byte(html))
 }
