66d83b55662001fc90a0c0b3ffe7e4dfdf23294b
Santiago Figueroa Aguirre <santiago.figueroa1@correo.icesi.edu.co>
2021-10-10 20:37:51 -0500

    First service consumed

--- src/main.go
+++ src/main.go
@@ -16,31 +16,6 @@ func main() {
 	http.HandleFunc("/upload", handler.UploadFilesHandler)
   http.HandleFunc("/files", handler.FetchFilesHandler)
   
-  http.HandleFunc("/archivo", archivo)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
-}
-
-func home(w http.ResponseWriter, r *http.Request){
-
-	html := "<!DOCTYPE html>";
-	html += "<head>";
-         html += "<title> Icesi drive </title>";
-	html += "</head>";
-	html += "<body>";
-	html += "<label> Capacidad:  </label> <p id=capacity> </p>";
-	html += "<form>"; 
-	html += "<label>Si desea subir un archivo presione</label>";
-	html += "<h3>Si desea subir un archivo presione</h3>";
-	html += "</form>";      
-
-    	html += "</body>";
-	html += "</html>";
-	
-	w.Write([]byte(html))
-}
-
-func archivo(w http.ResponseWriter, r *http.Request){
-
-	
 }
