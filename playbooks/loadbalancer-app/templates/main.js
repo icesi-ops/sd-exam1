0e1fde8555f5f1269702c710b998727a7699ffca
camilaleniss <mlmariacami2@gmail.com>
2021-10-16 18:18:13 -0500

    Add node js app logic to upload files in a folder

--- playbooks/loadbalancer-app/templates/main.js
+++ playbooks/loadbalancer-app/templates/main.js
@@ -1,6 +1,25 @@
 var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, '192.168.33.200');
-console.log('Server running at http://192.168.33.200:8080/');
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = '/etc/loadbalancer/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080,'192.168.33.200'); 
