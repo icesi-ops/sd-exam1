2a7b2055131393b1759acba6ce665a076d5384b1
Cesar Canales <45499862+Sleeptightt@users.noreply.github.com>
2022-05-08 19:44:36 -0500

    Added template for multiple front instances and fixed consul registration in app.py

--- front/app.py
+++ front/app.py
@@ -2,24 +2,27 @@ from flask import Flask, request, render_template
 from threading import Thread
 import requests
 import consul
+import socket
 
 app = Flask(__name__, template_folder='template')
 
+SERVICE_ADDRESS = socket.gethostbyname(socket.gethostname())
+
 @app.route("/")
 def hello():
-    return "Hello world!"
-
-@app.route("/upload-image", methods=["GET", "POST"])
-def upload():
     return render_template("upload_image.html")
 
-if __name__ == "__main__":
+def register():
     c = consul.Consul(host='consul', port=8500)
+    c.agent.service.register(
+        name='upload-image',
+        service_id='upload-image' + SERVICE_ADDRESS[len(SERVICE_ADDRESS)-1],
+        tags=['upload'],
+        address=SERVICE_ADDRESS,
+        port=5000)
+
+if __name__ == "__main__":
     # Register Service
-    c.agent.service.register('upload-image',
-                            service_id='upload-image',
-                            address='172.20.0.7',
-                            port=5000,
-                            tags=['upload'])
+    register()
     app.run(host='0.0.0.0',port = 5000) 
     
