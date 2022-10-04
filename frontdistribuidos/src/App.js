1d9e15fb926bcd0829b65bda0890cac34c470538
Andres Mayor <andresmayor03@gmail.com>
2022-10-03 19:56:29 -0500

    Frontend Terminado

--- frontdistribuidos/src/App.js
+++ frontdistribuidos/src/App.js
@@ -2,10 +2,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React,{useState , useEffect} from 'react'
 import { DataGrid } from '@mui/x-data-grid';
+import '@fontsource/roboto/300.css';
+import { Typography } from '@mui/material';
 
 function App() {
 
  const [archivos, setArchivos] = useState(null);
+ const [host,setHost]=useState("");
+ const [storage,setStorage]=useState("");
+ const [show,setShow]= useState(false);
+ const [message,setMessage]= useState("");
 
  const [rows,setRows]= useState([]);
 
@@ -38,15 +44,38 @@ function App() {
       const backResponse = await files.json(); 
       
       setRows(backResponse);
-      console.log(rows);
-      
     }
 
+  }
 
+  const getHost = async ()=>{
+    const host = await fetch ("http://localhost:8081/host",{
+      method:"GET"
+    });
+    if (!host.ok) {
+      throw new Error(`Error! status: ${host.status}`);
+    } else {
+      const backResponse = await host.json();
+      setHost(backResponse.response);
+     
+    }
+  }
+
+  const getStorage = async ()=>{
+    const storage = await fetch ("http://localhost:8081/storage",{
+      method:"GET"
+    });
+    if (!storage.ok) {
+      throw new Error(`Error! status: ${storage.status}`);
+    } else {
+      const backResponse = await storage.json();
+      setStorage(backResponse);
+    }
   }
 
 
   const insertarArchivo = async ()=>{
+    setMessage("");
     const file = new FormData();
     //Files por que asi se llama el parametro dentro de la api , ahi se pone el nombre del parametro del api el endpoitn
     
@@ -67,7 +96,10 @@ function App() {
     if (!response.ok) {
       throw new Error(`Error! status: ${response.status}`);
     } else {
-     
+      setMessage("El archivo se subio correctamente!");
+      setShow(true);
+      getHost();
+      getStorage();
     }
   
   
@@ -75,13 +107,18 @@ function App() {
 
   return (
     <div class="col-auto  p-5 text-center" overflowX="hidden" overflowY="hidden">
-      <br /><br />
-      <input type="file" name="files" onChange={(e) => subirArchivos(e.target.files)} />
-      <br></br>
-      <br></br>
+      <input type="file" name="files" onChange={(e) => subirArchivos(e.target.files)} /> 
+      <div style={{display:'flex', flexWrap:'wrap',justifyContent:'center',alignItems:'center'}}>
+      {show && 
+      <div>    
+      <Typography variant="h5" >Host: {host} </Typography>
+      <Typography variant="h5" > Almacenamiento: {storage} Bytes</Typography>
+      </div>
+      }
+      </div>  
       <button className="btn btn-primary" onClick={() => { insertarArchivo() }}>Insertar</button>
       <br></br>
-      <br></br>
+      <Typography variant="h5" color={'#64e01d'}> {message} </Typography>
       <div style={{ height: '70vh', width: '100%' }}>
         <DataGrid
           rows={rows}
@@ -94,5 +131,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
