4973dcc10d90d457b353bb7e17390aa2fee6c5b6
Andres Mayor <andresmayor03@gmail.com>
2022-10-19 10:20:31 -0500

    .

--- backendSd/src/main/java/com/example/backendSd/services/FileServices.java
+++ backendSd/src/main/java/com/example/backendSd/services/FileServices.java
@@ -37,7 +37,7 @@ public class FileServices  implements IUploadFile {
 
         String resultService = "";
        // String storageConnectionAzure="DefaultEndpointsProtocol=https;AccountName=accountfiles1;AccountKey=EWzNYtXFeN2/Nyej32bieuKlpDUylWKV7Fd0SZWI/o6dByPww457sVGA06DVAYD/qwjz7d4UoOl7+AStkjteGQ==;EndpointSuffix=core.windows.net";
-        String storageConnectionAzure ="DefaultEndpointsProtocol=https;AccountName=storageaja;AccountKey=zas79b2Z+KRKRd23pnRXxPZ5V14KH6jw3MfBG+itVEpWwpdQdxRwzBDot1wIiydb0tyJYqtDYW91+ASt+f21uA==;EndpointSuffix=core.windows.net";
+        String storageConnectionAzure ="DefaultEndpointsProtocol=https;AccountName=storageaja;AccountKey=OkrwT5fWdIytFktPk17JIazvSbYvVhlO7Hl5w3mxWBD03Fib9QZKEWEyBZMc3aVWWX9LYX0dKEKW+AStQ66MCg==;EndpointSuffix=core.windows.net";
         String nameContainer = "files";
 
         try{
@@ -81,7 +81,7 @@ public class FileServices  implements IUploadFile {
     @Override
     public long Storage() {
 
-        String storageConnectionAzure ="DefaultEndpointsProtocol=https;AccountName=storageaja;AccountKey=zas79b2Z+KRKRd23pnRXxPZ5V14KH6jw3MfBG+itVEpWwpdQdxRwzBDot1wIiydb0tyJYqtDYW91+ASt+f21uA==;EndpointSuffix=core.windows.net";
+        String storageConnectionAzure ="DefaultEndpointsProtocol=https;AccountName=storageaja;AccountKey=OkrwT5fWdIytFktPk17JIazvSbYvVhlO7Hl5w3mxWBD03Fib9QZKEWEyBZMc3aVWWX9LYX0dKEKW+AStQ66MCg==;EndpointSuffix=core.windows.net";
         String nameContainer = "files";
 
         long size =0;
