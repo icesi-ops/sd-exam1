d1ddef7d3d6b0dd98edd2e8d9bc98eaf7022a756
Benjamin Silva <benjamin.silva@insight4.tech>
2023-03-22 15:37:35 -0500

    remove old folders

--- 2020-2/README.md
+++
@@ -1,1 +0,0 @@
-Here there are the midters and final project used for smester 2020-1
--- 2021-2/midterm3/IcesiHealth/backend/scripts/database.js
+++
@@ -1,3 +0,0 @@
-var nano = require('nano');
-
-module.exports = nano(process.env.DATABASE_URL || 'http://127.0.0.1:5984');
