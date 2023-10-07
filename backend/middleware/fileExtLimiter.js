b7610062bfbab407d073756bf7ca9a2eb0c8c485
Kevin <kamcak@hotmail.es>
2023-10-07 11:01:47 -0500

    Backend V1.0.0

---
+++ backend/middleware/fileExtLimiter.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+const fileExtLimiter = (allowedExtArray) => {
+  return (req, res, next) => {
+    const files = req.files;
+
+    const fileExtensions = [];
+    Object.keys(files).forEach((key) => {
+      fileExtensions.push(path.extname(files[key].name));
+    });
+
+    // Las extensiones de los archivos son permitidas?
+    const allowed = fileExtensions.every((ext) =>
+      allowedExtArray.includes(ext)
+    );
+    if (!allowed) {
+      const message =
+        `Upload failed. Only ${allowedExtArray.toString()} files allowed.`.replaceAll(
+          ",",
+          ", "
+        );
+
+      return res.status(422).json({ status: "error", message });
+    }
+
+    next();
+  };
+};
+
+module.exports = fileExtLimiter;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,48 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
+const filesPayloadExists = require("./middleware/filesPayloadExists");
+const fileExtLimiter = require("./middleware/fileExtLimiter");
+const fileSizeLimiter = require("./middleware/fileSizeLimiter");
+
+const PORT = process.env.PORT || 3500;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo");
+  res.sendFile(path.join(__dirname, "index.html"));
+  // res.send("Hola mundo");
 });
 
-app.post("/files", (req, res) => {
-    res.send("Hola mundo");
-  });
+app.post(
+  "/upload",
+  fileUpload({ createParentPath: true }),
+  filesPayloadExists,
+  // fileExtLimiter([".png", ".jpg", ".jpeg"]),
+  fileExtLimiter([".pdf"]),
+  fileSizeLimiter,
+  (req, res) => {
+    const files = req.files;
+    console.log(files);
 
-app.listen(8001, (req, res) => {
-  console.log("Server Started");
+    Object.keys(files).forEach((key) => {
+      const filepath = path.join(__dirname, "files", files[key].name);
+      files[key].mv(filepath, (err) => {
+        if (err) return res.status(500).json({ status: "error", message: err });
+      });
+    });
+
+    return res.json({
+      status: "success",
+      message: Object.keys(files).toString(),
+    });
+  }
+);
+
+// app.post("/files", (req, res) => {
+//   res.send("Hola mundo");
+// });
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
