89741cc612dc4a2316ae628828b4c121c19f35fb
Andres Orozco <juanxxi2015@gmail.com>
2023-10-07 15:17:37 -0500

    Fix: Changes due to usage of angular

--- upload-file-front/src/app/pages/upload-file/upload-file.component.ts
+++ upload-file-front/src/app/pages/upload-file/upload-file.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-upload-file',
@@ -10,12 +13,13 @@ import { Router } from '@angular/router';
 export class UploadFileComponent { 
 
   router: Router = inject(Router);
-
   uploadForm: FormGroup = new FormGroup({
     file: new FormControl('')
   });
 
   showModal = false;
+
+  constructor(private http: HttpClient) {}
 
   openModal() {
     this.showModal = true;
@@ -25,19 +29,36 @@ export class UploadFileComponent {
     this.showModal = false;
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.uploadForm.value);
-    const form = document.getElementById('uploadForm')
-    const selectedFile = this.uploadForm.value;
-
-    if (!selectedFile) {
-      alert("Debes seleccionar un archivo para cargar.");
+  
+    // Accede al FormControl 'file'
+    const fileControl = this.uploadForm.get('file');
+  
+    // Verifica si el FormControl tiene un valor y si ese valor no es nulo
+    if (fileControl && fileControl.value) {
+      const selectedFile = fileControl.value;
+  
+      // Continúa con el resto de la lógica aquí
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+  
+      this.http.post('http://localhost:3500/upload', formData).pipe(
+        catchError((error) => {
+          console.error("Error al cargar el archivo:", error);
+          alert("Error al cargar el archivo. Por favor, inténtalo de nuevo.");
+          return of(null);
+        })
+      ).subscribe((response) => {
+        if (response) {
+          alert("Archivo cargado con éxito");
+          this.closeModal();
+          this.router.navigate(["show"]);
+        }
+      });
     } else {
-     
-
-      alert("Cargado");
-      this.closeModal();
-      this.router.navigate(["show"]);
+      alert("Debes seleccionar un archivo para cargar.");
     }
   }
+  
 }
