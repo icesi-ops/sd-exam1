d2577ae8019cae83cf7b3bc056f8254c70dcf0ef
Kevin <kamcak@hotmail.es>
2023-11-25 12:06:46 -0500

    backend and nas wip

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const SMB2 = require("smb2");
 
 const filesPayloadExists = require("./middleware/filesPayloadExists");
 const fileExtLimiter = require("./middleware/fileExtLimiter");
@@ -14,6 +15,13 @@ app.get("/", (req, res) => {
   // res.send("Hola mundo");
 });
 
+const smb2Client = new SMB2({
+  share: "\\\\samba\\Compartido",
+  domain: "WORKGROUP",
+  username: "root",
+  password: "",
+});
+
 app.post(
   "/upload",
   fileUpload({ createParentPath: true }),
@@ -23,13 +31,15 @@ app.post(
   fileSizeLimiter,
   (req, res) => {
     const files = req.files;
-    console.log(files);
 
     Object.keys(files).forEach((key) => {
       const filepath = path.join(__dirname, "files", files[key].name);
-      files[key].mv(filepath, (err) => {
+      smb2Client.writeFile(filepath, files[key].data, function (err) {
         if (err) return res.status(500).json({ status: "error", message: err });
       });
+      // files[key].mv(filepath, (err) => {
+      //   if (err) return res.status(500).json({ status: "error", message: err });
+      // });
     });
 
     return res.json({
