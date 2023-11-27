0b06ccfc7f19201cb648c47e8935e76a4270de29
Gonzalo De Varona <gonzalodevarona@Gonzalos-MacBook-Pro.local>
2023-11-27 12:38:44 -0500

    feat: now I modularized the code even more for the game form

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -1,39 +1,50 @@
 import { GameType } from "../schemas/GameSchema"
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { useState } from 'react'
+import GameFormModal from "./GameFormModal";
+import { CardMedia, Typography, Button, CardContent, CardActions, Card } from '@mui/material'
 import { capitalizeFirstLetter } from "../utils/CommonMethods";
 
 function GameCard(props: { id: string, name: string, release_year: number }) {
 
-  const game : GameType = props;
+  const game: GameType = props;
+
+  const [openForm, setOpenForm] = useState(false)
+
+  function displayForm() {
+    setOpenForm(true)
+  }
+  function closeForm() {
+    setOpenForm(false)
+  }
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {capitalizeFirstLetter(game.name)}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          ID: { game.id }
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Release year: { game.release_year }
-        </Typography>
-      </CardContent>
-      <CardActions sx={{display:"flex", justifyContent:'center'}}>
-        <Button variant='contained' size="small">Edit</Button>
-        <Button variant='contained' color='error' size="small">Delete</Button>
-      </CardActions>
-    </Card>
+    <>
+      
+      <GameFormModal open={openForm} closeForm={closeForm}  action='edit' game={game}/>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {capitalizeFirstLetter(game.name)}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            ID: {game.id}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Release year: {game.release_year}
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ display: "flex", justifyContent: 'center' }}>
+          <Button onClick={displayForm} variant='contained' size="small">Edit</Button>
+          <Button variant='contained' color='error' size="small">Delete</Button>
+        </CardActions>
+      </Card>
+    </>
   )
 }
 
--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
 
-const baseURL = 'https://api.example.com';
+const baseURL = 'http://localhost:8080';
 
 const instance: AxiosInstance = axios.create({
   baseURL
-  
 });
 
 export default instance;
