f0170c6c46df9ac3a2efe45b3e7fddf383e47c15
Gonzalo De Varona <gonzalodevarona@Gonzalos-MacBook-Pro.local>
2023-11-27 00:35:33 -0500

    hotfix: added routing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,11 @@
-
+import GameCard from "../components/GameCard"
+import GameForm from "../components/GameForm"
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <GameCard name='melo' release_year={2} id='a' />
+      <GameForm action='add' game={undefined}></GameForm>
+    </>
   )
 }
 
