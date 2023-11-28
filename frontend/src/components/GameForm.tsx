84117d4fa8754f5e250a459f19e06456f11917d8
Gonzalo De Varona <gonzalodevarona@Gonzalos-MacBook-Pro.local>
2023-11-27 22:44:57 -0500

    hotfix: removed unused control

--- frontend/src/components/GameForm.tsx
+++ frontend/src/components/GameForm.tsx
@@ -38,7 +38,7 @@ function GameForm(props: GameFormPropsType) {
     handleSubmit,
     register,
     formState: { errors },
-    control,
+    // control,
     watch
   } = useForm({
     defaultValues: preloadedData
