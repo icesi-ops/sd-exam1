93b612a1fb0b096c7f738330fb6b0c1fb7f5bd64
DaniBonica001 <danielabc_0106@hotmail.com>
2024-03-05 00:46:10 -0500

    front with nginx

--- app-back/main.go
+++ app-back/main.go
@@ -1,5 +1,38 @@
-// handleAddBook maneja la solicitud para agregar un nuevo libro
-func handleAddBook(w http.ResponseWriter, r *http.Request) {
+package main
+
+import (
+    "context"
+    "encoding/json"
+    "log"
+    "net/http"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+
+)
+
+// Define una estructura para el libro
+type Book struct {
+    Title  string `json:"title"`
+    Size string `json:"size"`
+    Type string `json:"type"`
+    // Agrega otros campos según tus necesidades
+}
+
+var client *mongo.Client
+
+// Inicializa la conexión a MongoDB
+func init() {
+    clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017") // Asegúrate de cambiar la URI si es necesario
+    var err error
+    client, err = mongo.Connect(context.Background(), clientOptions)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+// Endpoint para guardar un libro en la base de datos
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
     var book Book
     err := json.NewDecoder(r.Body).Decode(&book)
     if err != nil {
@@ -7,27 +40,55 @@ func handleAddBook(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    books = append(books, book)
-    fmt.Fprintf(w, "Libro '%s' agregado correctamente", book.Title)
+    log.Println("Recibido libro:")
+    log.Println(book)
+
+    collection := client.Database("booksdb").Collection("books")
+    _, err = collection.InsertOne(context.Background(), book)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
 }
 
-// handleListBooks maneja la solicitud para listar todos los libros
-func handleListBooks(w http.ResponseWriter, r *http.Request) {
+// Endpoint para obtener todos los libros de la base de datos
+func getAllBooksHandler(w http.ResponseWriter, r *http.Request) {
+    collection := client.Database("booksdb").Collection("books")
+    cur, err := collection.Find(context.Background(), bson.D{})
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer cur.Close(context.Background())
+
+    var books []Book
+    for cur.Next(context.Background()) {
+        var book Book
+        err := cur.Decode(&book)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        books = append(books, book)
+    }
+
+    w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(books)
 }
 
-// handleHealthCheck maneja la solicitud de comprobación de salud del sistema
-func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Sistema en funcionamiento")
+// Endpoint para verificar la salud del servicio
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "application/json")
+    response := map[string]bool{"ok": true}
+    json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-    // Endpoints
-    http.HandleFunc("/add_book", handleAddBook)
-    http.HandleFunc("/list_books", handleListBooks)
-    http.HandleFunc("/health", handleHealthCheck)
-
-    // Servidor
-    fmt.Println("Servidor en funcionamiento en el puerto :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    http.HandleFunc("/api/upload", uploadHandler)
+    http.HandleFunc("/api/books", getAllBooksHandler)
+    http.HandleFunc("/api/health", healthCheckHandler)
+    log.Fatal(http.ListenAndServe(":3000", nil))
 }
--- app-front/src/App.jsx
+++ app-front/src/App.jsx
@@ -5,62 +5,72 @@ function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploadedFiles, setUploadedFiles] = useState([]);
 
-  // Función para obtener la lista de archivos cargados
-  const fetchUploadedFiles = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/api/files');
-      if (response.ok) {
-        const data = await response.json();
-        setUploadedFiles(data);
-      } else {
-        console.error('Error al obtener archivos del servidor');
-      }
-    } catch (error) {
-      console.error('Error en la solicitud:', error);
-    }
-  };
-
-  // Cargar la lista de archivos al cargar la página
-  useEffect(() => {
-    fetchUploadedFiles();
-  }, []); // Empty array as the dependency to run once on component mount
-
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
+    handleSubmit();
   };
 
   const handleSubmit = async () => {
     if (selectedFile) {
-      const formData = new FormData();
-      formData.append('file', selectedFile);
+
+      console.log('Archivo seleccionado:', selectedFile);
+      const newFile = {
+        name: selectedFile.name,
+        size: selectedFile.size,
+        type: selectedFile.type,
+      };
 
       try {
-        const response = await fetch('http://localhost:8080/api/upload', {
+        const response = await fetch('http://backend:3000/api/upload', {
           method: 'POST',
-          body: formData,
+          headers: {
+            'Content-Type': 'application/json', // Tipo de contenido JSON
+          },
+          body: JSON.stringify(newFile),
         });
 
         if (response.ok) {
           console.log('Archivo enviado con éxito al servidor');
-          // Refetch the list of uploaded files after successful upload
           fetchUploadedFiles();
+
         } else {
           console.error('Error al enviar archivo al servidor');
         }
       } catch (error) {
         console.error('Error en la solicitud:', error);
       }
+
     } else {
       alert('Por favor selecciona un archivo');
     }
   };
+
+
+  // Cargar la lista de archivos al cargar la página
+  useEffect(() => {
+    fetchUploadedFiles();
+  }, []); // Empty array as the dependency to run once on component mount
+
+
+  const fetchUploadedFiles = async () => {
+    try {
+        const response = await fetch('http://backend:3000/api/books');
+        if (!response.ok) {
+            throw new Error('Error al obtener archivos del servidor');
+        }
+        const data = await response.json();
+        setUploadedFiles(data);
+    } catch (error) {
+        console.error('Error en la solicitud:', error.message);
+    }
+};
+
 
   return (
     <div className="App">
       <h1>Sube tu archivo</h1>
       <div className="card">
         <input type="file" onChange={handleFileChange} />
-        <button onClick={handleSubmit}>Subir archivo</button>
       </div>
       <div className="table-container">
         <h2>Archivos cargados</h2>
