051ee35e6d23075d43124a29ec6fa8713854f7f2
estebanm1812 <EstebanMendoza02@hotmail.com>
2024-03-10 22:43:58 -0500

    the other functionality works

--- app-back/main.go
+++ app-back/main.go
@@ -40,7 +40,7 @@ var s *smb2.Session
 // Inicializa la conexión a MongoDB y Samba
 func init() {
 
-    // Conexión a Samba
+	// Conexión a Samba
 	conn, err_samba := net.Dial("tcp", "my-samba-container:445")
 	if err_samba != nil {
 		panic(err_samba)
@@ -86,141 +86,141 @@ func init() {
 
 // Endpoint para guardar un libro en la base de datos
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-    var book Book
-    log.Println("Recibiendo libro...")
+	var book Book
+	log.Println("Recibiendo libro...")
 
-    // Parsear el formulario
-    err := r.ParseMultipartForm(10 << 20) // 10 MB limit
-    if err != nil {
-        log.Println("Error parsing form: ", err)
-        http.Error(w, "Error parsing form", http.StatusBadRequest)
-        return
-    }
-    file, _, err := r.FormFile("file")
-    log.Println("File: ", file)
+	// Parsear el formulario
+	err := r.ParseMultipartForm(10 << 20) // 10 MB limit
+	if err != nil {
+		log.Println("Error parsing form: ", err)
+		http.Error(w, "Error parsing form", http.StatusBadRequest)
+		return
+	}
+	file, _, err := r.FormFile("file")
+	log.Println("File: ", file)
 
-    if err != nil {
-        log.Println("Error retrieving file: ", err)
-        http.Error(w, "Error retrieving file", http.StatusBadRequest)
-        return
-    }
+	if err != nil {
+		log.Println("Error retrieving file: ", err)
+		http.Error(w, "Error retrieving file", http.StatusBadRequest)
+		return
+	}
 
-    log.Println("File: ", err)
-    defer file.Close()
+	log.Println("File: ", err)
+	defer file.Close()
 
-    // Obtener los valores de los campos del formulario
-    name := r.FormValue("name")
-    size := r.FormValue("size")
-    fileType := "." + r.FormValue("type")
+	// Obtener los valores de los campos del formulario
+	name := r.FormValue("name")
+	size := r.FormValue("size")
+	fileType := "." + r.FormValue("type")
 
-    if name == "" || size == "" || fileType == "" {
-        log.Println("Missing required fields")
-        http.Error(w, "Missing required fields", http.StatusBadRequest)
-        return
-    }
+	if name == "" || size == "" || fileType == "" {
+		log.Println("Missing required fields")
+		http.Error(w, "Missing required fields", http.StatusBadRequest)
+		return
+	}
 
-    // Crear la estructura Book
-    book = Book{Name: name, Size: size, Type: fileType}
-    log.Println(book)
+	// Crear la estructura Book
+	book = Book{Name: name, Size: size, Type: fileType}
+	log.Println(book)
 
-    //Guardar el libro en la base de datos
-    _, err = collection.InsertOne(context.Background(), book)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	//Guardar el libro en la base de datos
+	_, err = collection.InsertOne(context.Background(), book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    // Conexión a Samba
-    conn, err_samba := net.Dial("tcp", "my-samba-container:445")
-    if err_samba != nil {
-        panic(err_samba)
-    }
-    defer conn.Close()
+	// Conexión a Samba
+	conn, err_samba := net.Dial("tcp", "my-samba-container:445")
+	if err_samba != nil {
+		panic(err_samba)
+	}
+	defer conn.Close()
 
-    d := &smb2.Dialer{
-        Initiator: &smb2.NTLMInitiator{
-            User:     "myuser",
-            Password: "mypassword",
-        },
-    }
+	d := &smb2.Dialer{
+		Initiator: &smb2.NTLMInitiator{
+			User:     "myuser",
+			Password: "mypassword",
+		},
+	}
 
-    s, err_samba = d.Dial(conn)
-    if err_samba != nil {
-        panic(err_samba)
-    }
-    fmt.Println("para ver si da error ", err_samba)
-    log.Println("Conexión con el samba")
+	s, err_samba = d.Dial(conn)
+	if err_samba != nil {
+		panic(err_samba)
+	}
+	fmt.Println("para ver si da error ", err_samba)
+	log.Println("Conexión con el samba")
 
-    log.Println("mount antes de : ")
-    if s == nil {
-        log.Println("s es nil")
-    }
+	log.Println("mount antes de : ")
+	if s == nil {
+		log.Println("s es nil")
+	}
 
-    // Montar el recurso compartido
-    fs, err := s.Mount("\\\\my-samba-container\\shared")
-    log.Println("mount: ")
-    if err != nil {
-        panic(err)
-    }
+	// Montar el recurso compartido
+	fs, err := s.Mount("\\\\my-samba-container\\shared")
+	log.Println("mount: ")
+	if err != nil {
+		panic(err)
+	}
 
-    log.Println("mount pasado el if ")
+	log.Println("mount pasado el if ")
 
-    // Crear un buffer de bytes para almacenar el contenido del archivo
-    var buffer bytes.Buffer
+	// Crear un buffer de bytes para almacenar el contenido del archivo
+	var buffer bytes.Buffer
 
-    // Leer el contenido del archivo y escribirlo en el buffer
-    _, err = io.Copy(&buffer, file)
-    if err != nil {
-        panic(err)
-    }
+	// Leer el contenido del archivo y escribirlo en el buffer
+	_, err = io.Copy(&buffer, file)
+	if err != nil {
+		panic(err)
+	}
 
-    // Convertir el buffer de bytes a un slice de bytes
-    archivoBytes := buffer.Bytes()
+	// Convertir el buffer de bytes a un slice de bytes
+	archivoBytes := buffer.Bytes()
 
-    // Ahora "archivoBytes" contiene el contenido del archivo como un slice de bytes ([]byte)
-    fmt.Println("Contenido del archivo como slice de bytes: No lo vamos a mostrar de nuevo")
+	// Ahora "archivoBytes" contiene el contenido del archivo como un slice de bytes ([]byte)
+	fmt.Println("Contenido del archivo como slice de bytes: No lo vamos a mostrar de nuevo")
 
-    // Crear o abrir un archivo en el servidor Samba
-    f, err := fs.Create(name + fileType)
-    if err != nil {
-        panic(err)
-    }
-    defer fs.Remove(name +fileType)
-    defer f.Close()
+	// Crear o abrir un archivo en el servidor Samba
+	f, err := fs.Create(name + fileType)
+	if err != nil {
+		panic(err)
+	}
+	defer fs.Remove(name + fileType)
+	defer f.Close()
 
-    smbFile, err := fs.Open(name + fileType)
-    if err != nil {
-        panic(err)
-    }
-    defer file.Close()
+	smbFile, err := fs.Open(name + fileType)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-    fmt.Println("Pasa del fs.Open")
+	fmt.Println("Pasa del fs.Open")
 
-    // Copiar el contenido del archivo multipart al archivo en el servidor Samba
-    _, err = io.Copy(smbFile, file)
-    if err != nil {
-        panic(err)
-    }
+	// Copiar el contenido del archivo multipart al archivo en el servidor Samba
+	_, err = io.Copy(smbFile, file)
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Println("Pasa del io.Copy")
+	fmt.Println("Pasa del io.Copy")
 
-    // Escribir contenido en el archivo
-    err = fs.WriteFile(name+ fileType, archivoBytes, 0644)
-    if err != nil {
-        panic(err)
-    }
+	// Escribir contenido en el archivo
+	err = fs.WriteFile(name+fileType, archivoBytes, 0644)
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Println("Pasa del fs.WriteFile")
+	fmt.Println("Pasa del fs.WriteFile")
 
-    fmt.Println("Archivo guardado en el servidor Samba.")
-    defer fs.Umount()
-    log.Println("Desmontado el recurso compartido")
-    defer s.Logoff()
-    w.WriteHeader(http.StatusOK)
-    log.Println("Desconectado del servidor Samba")
-    panic("terminate")
+	w.WriteHeader(http.StatusOK)
+	fmt.Println("Archivo guardado en el servidor Samba.")
+	defer fs.Umount()
+	log.Println("Desmontado el recurso compartido")
+	defer s.Logoff()
+	//w.WriteHeader(http.StatusOK)
+	log.Println("Desconectado del servidor Samba")
+	panic("terminate")
 }
-
 
 // Endpoint para obtener todos los libros de la base de datos
 func getAllBooksHandler(w http.ResponseWriter, r *http.Request) {
@@ -240,6 +240,34 @@ func getAllBooksHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		books = append(books, book)
+	}
+	conn, err := net.Dial("tcp", "my-samba-container:445")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	d := &smb2.Dialer{
+		Initiator: &smb2.NTLMInitiator{
+			User:     "admin",
+			Password: "password1",
+		},
+	}
+
+	s, err := d.Dial(conn)
+	if err != nil {
+		panic(err)
+	}
+	defer s.Logoff()
+
+	names, err := s.ListSharenames()
+	if err != nil {
+		fmt.Print("Es Nulo el ListSharenames")
+		panic(err)
+	}
+
+	for _, name := range names {
+		fmt.Println("Esto se debe estar imprimiendo " + name)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
