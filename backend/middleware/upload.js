e7c5cb74a4c183d33eb241de0e6523ed44d92eb4
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-03-12 10:22:12 -0500

    change: multer implementation

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,21 +1,11 @@
 const util = require("util");
 const multer = require("multer");
-const sendFile = require("./samba");
 
 const maxSize = 2 * 1024 * 1024;
 
-const directoryPath = __basedir + "/resources/static/assets/uploads/";
-
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, __basedir + "/resources/static/assets/uploads/");
-    },
     filename: (req, file, cb) => {
-        console.log(file.originalname);
         cb(null, file.originalname);
-        sendFile(directoryPath + file.originalname, file.originalname)
-            .then(r => console.log(r))
-            .catch(e => console.log(e));
     },
 });
 
@@ -24,5 +14,4 @@ let uploadFile = multer({
     limits: { fileSize: maxSize },
 }).single("file");
 
-let uploadFileMiddleware = util.promisify(uploadFile);
-module.exports = uploadFileMiddleware;
+module.exports = uploadFile;
