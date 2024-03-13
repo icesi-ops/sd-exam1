2f320d97b240ecacd53f40609f81ab83e8531c99
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-03-13 16:27:01 -0500

    add: destination in multer diskStorage

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,9 +1,11 @@
-const util = require("util");
 const multer = require("multer");
 
 const maxSize = 2 * 1024 * 1024;
 
 let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __basedir + "/resources/static/assets/uploads/");
+    },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
     },
