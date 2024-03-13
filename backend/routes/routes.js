5917a377d25d54b1e6f65c255c427fa00b4544ef
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-03-13 16:39:16 -0500

    add: delete and put routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,6 +8,8 @@ let routes = (app) => {
     router.post("/upload", upload, fileController.upload);
     router.get("/files", fileController.getListFiles);
     router.get("/files/:name", fileController.download);
+    router.delete("/files/:id/:rev", fileController.removeFile);
+    router.put("/files", fileController.updateFile);
     router.get("/db", dbController.listDbs);
     router.post("/db/:db", dbController.createDb);
     router.get("/health", (req, res) => {
